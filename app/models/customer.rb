14555409ad3928eb045b9be3a769836e07fdb966
BenLooper <benlooper08@gmail.com>
2020-09-02 11:10:38 -0500

    submission for deadline

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,44 @@
 class Customer
   attr_accessor :given_name, :family_name
 
+  @@all = []
+
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+
+    self.class.all.push(self)
   end
 
   def full_name
     "#{given_name} #{family_name}"
   end
+
+  def reviews
+    Review.all.select { |review| review.customer == self}
+  end 
+
+  def restaurants 
+    self.reviews.collect{ |review| review.restaurant}.uniq
+  end 
+
+  def add_review(restaurant,rating)
+    Review.new(self,restaurant,rating)
+  end 
+
+  def num_reviews
+    self.reviews.count
+  end 
+
+  def self.find_by_name(name)
+    self.all.find{ |customer| customer.full_name == name}
+  end 
+
+  def self.find_by_given_name(name)
+    self.all.select{ |customer| customer.given_name == name}
+  end 
+
+  def self.all
+    @@all
+  end 
 end
