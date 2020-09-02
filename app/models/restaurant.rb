e9c195de4da8b15f7b0ed96ea8518f70793c60f2
wyounis25 <github email address>
2020-09-02 11:42:34 -0500

    submission for deadline

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,57 @@
 class Restaurant
   attr_reader :name
 
+
+
+
+  @@all = []
   def initialize(name)
     @name = name
+    self.class.all << self 
   end
+
+  def self.all
+    @@all
+  end 
+
+
+    def reviews
+
+      Review.all.select do |restaurant|
+        restaurant.resturant == self 
+      end
+    end 
+
+    def customers
+      review = Review.all.select do |restaurant|
+        restaurant.resturant == self 
+      end 
+      review.all.collect do |customer|
+        customer
+      end 
+      
+
+    customer.uniq
+    end 
+
+    def average_star_rating 
+      total = Review.all.sum do |rating|
+        rating.rating == self 
+      end 
+      total / self.all.count
+    end 
+
+
+
+
+
+ 
+
+
+
+
+
+
+
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,37 @@
 class Review
   
-end
+
+    attr_reader :customer, :resturant, :rating
+    @@all = []
+    def initialize (customer,resturant,rating)
+        @customer = customer
+        @resturant = resturant
+        @rating = rating 
+        self.class.all << self 
+    end
+    def self.all
+        @@all
+    end 
+
+    def rating
+    end
+
+    # def customer 
+
+    #     Review.all.select do |reviews|
+            
+    #       reviews == self 
+    #     end 
+    #   end 
+
+    #   def restaurant
+    #     customer.collect do |resturants|
+    #         resturants.resturant
+    #     end 
+    # end 
+
+    
+
+
+    
+end  
