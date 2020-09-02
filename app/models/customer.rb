b0b2a53506bb8cf96ce2ed3bf08a0e0a0523594a
MSarfarazH <m.s.hussain@outlook.com>
2020-09-02 11:42:02 -0500

    submission for deadline

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,29 @@
 class Customer
   attr_accessor :given_name, :family_name
+  @@all = []
 
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+   
+    self.class.all << self
   end
+
+  def review
+    Review.all.select{ |rating| rating.customer == self}
+  end
+
+  def restaurants
+    self.review.collect do | review |
+      #binding.pry
+      review.restaurant
+  end    
 
   def full_name
     "#{given_name} #{family_name}"
+  end
+
+  def self.all
+    @@all
   end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,14 @@
+require 'pry'
 class Restaurant
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
   end
-
+  # binding.pry
+  # 0
+ def self.all
+  @@all
+ end
 end
