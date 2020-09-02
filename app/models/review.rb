7f1dbfab109c8076937980e6531bef3eae38ab3b
Far.k <farzana.mkarim@gmail.com>
2020-09-02 11:59:09 -0500

    .

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,30 @@
 class Review
+    attr_reader :customer, :restaurant
+    attr_accessor :rating
+   @@all = []
+
+    def initialize(customer, restaurant, rating)
+        @customer = customer
+        @restaurant = restaurant
+        @rating = rating
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    
+    def customer
+         self.all.collect do |rev|
+             rev.customer
+         end
+     end
+
+     def restaurant
+        self.all.collect do |rev|
+            rev.restaurant
+        end
+     end
   
 end
