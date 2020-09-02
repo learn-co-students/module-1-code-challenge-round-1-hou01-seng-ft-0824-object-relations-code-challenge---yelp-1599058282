a5ffe78a2e7c5af282c4fcb1cff2f9a959ab85d8
stephenandersondev <stephenandersondev@gmail.com>
2020-09-02 10:52:31 -0500

    submission for deadline

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,21 @@
 class Restaurant
+  
   attr_reader :name
 
   def initialize(name)
     @name = name
+  end
+
+  def reviews
+    Review.all.select {|review| review.restaurant == self}
+  end
+
+  def customers
+    (self.reviews.collect {|review| review.customer}).uniq
+  end
+
+  def average_star_rating
+  (self.reviews.sum {|review| review.rating}) / self.reviews.count
   end
 
 end
