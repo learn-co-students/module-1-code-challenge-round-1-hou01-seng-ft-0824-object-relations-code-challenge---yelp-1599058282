c51039e6b8034d1476f7672cb24bad500d929aa5
github username <github email address>
2020-09-02 11:40:20 -0500

    submission for deadline

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,42 @@
 class Customer
   attr_accessor :given_name, :family_name
 
+  @@all =[]
+
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+    self.class.all.push(self)
+  end
+  def self.all
+    @@all
   end
 
   def full_name
     "#{given_name} #{family_name}"
   end
-end
+
+  def customer_reviews #i used so that i didnt have to write this code twice 
+    Review.all.select{|review| review.customer == self}.collect{|review| review.restaurant}
+  end 
+
+  def restraunts
+    self.customer_reviews.uniq
+  end 
+
+  def add_review(restaurant, rating)
+    Review.new(self, restaurant, rating)
+  end 
+
+  def num_reviews
+    customer_reviews.length
+  end 
+
+  def self.find_by_name(name)
+    self.all.find{|customer| name.include? customer.family_name} 
+  end
+
+  def self.find_all_by_given_name(name)
+    self.all.select{|customer| name.include? customer.family_name} 
+  end
+end 
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,4 +5,15 @@ class Restaurant
     @name = name
   end
 
+  def reviews
+    Review.all.select{|review| review.restaurant == self}
+  end
+
+  def customers
+    reviews.collect{|review| review.customer}
+  end 
+
+  def average_star_rating
+    (reviews.collect{|review| review.rating}.sum)/reviews.length 
+  end   
 end
